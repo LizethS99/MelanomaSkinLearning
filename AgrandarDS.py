b17fd4f4e6dc2a263ed46771e869450b4eda940e
BJMB2101 <83789372+BJMB2101@users.noreply.github.com>
2023-09-26 23:17:03 -0600

    Update AgrandarDS.py
    Se agregó el parametro index=false en la exportación a excel evitando la columna de los indices, de igual manera se agregó una condicional al guardar los valores, de tal manera que si se tiene un valor NaN lo cambie por una casilla vacia.

--- AgrandarDS.py
+++ AgrandarDS.py
@@ -1,17 +1,70 @@
+#Generación de data set completo
 import cv2
 import os
+import pandas as pd
+def formatNum(n):
+    strn = ""
+    mod = 1000
+    while n%mod==n:
+        strn+="0"
+        mod/=10
+    return strn+str(n)
+
 cont = 0
 img_melanoma = []
 imagen = []
-for img in os.listdir('./PH2'):
+idxDF = 0
+bsq_row = ""
+num=0
+df_ph2 = pd.read_excel("./GIT/NewPH2.xlsx")
+cols = df_ph2.columns.tolist()
+df_Nph2 = pd.DataFrame(columns=cols)
+for img in os.listdir('./PH2P'):
+    bsq_row=img[:6]
+    if num==0:
+        num=int(bsq_row[3:6])
+    bsq_idx = df_ph2.loc[df_ph2['Image Name']==bsq_row].to_dict()
+    bsq_idx[cols[0]] = "IMD"+formatNum(num)
+    values = []
+    for i in cols:
+        if cols.index(i) == 0:
+            values.append(bsq_idx[i])
+        else:
+            dicAux = list(bsq_idx[i].values())
+            if str(dicAux[0])=="nan":
+                values.append("")
+            else:
+                values.append((str(dicAux[0])))
+    df_Nph2.loc[len(df_Nph2)] = values
+    values.clear()
+    num+=1
     img_melanoma.append(img[:6])
+    if num==0:
+        num=int(img[3:6])
     if img.endswith('.bmp'):
+        
         # Lee la imagen que deseas rotar
-        imagen = cv2.imread(os.path.join('./PH2', img))
+        imagen = cv2.imread(os.path.join('./PH2P', img))
+        
+        
+        cv2.imwrite(f"./NewDataSet3/{img[:3]+formatNum(num)}.bmp", imagen)
         # Especifica el ángulo de rotación (en grados)
         angulo_rotacion = 18.8947368  # Cambia el ángulo según tus necesidades
 
         while angulo_rotacion < 360:
+            bsq_idx[cols[0]] = "IMD"+formatNum(num)
+            for i in cols:
+                if cols.index(i) == 0:
+                    values.append(bsq_idx[i])
+                else:
+                    dicAux = list(bsq_idx[i].values())
+                    if str(dicAux[0])=="nan":
+                        values.append("")
+                    else:
+                        values.append((str(dicAux[0])))
+            df_Nph2.loc[len(df_Nph2)] = values
+            values.clear()
+            num+=1
             # Obtiene el centro de la imagen
             altura, ancho = imagen.shape[:2]
             centro = (ancho // 2, altura // 2)
@@ -23,11 +76,13 @@ for img in os.listdir('./PH2'):
             imagen_rotada = cv2.warpAffine(imagen, matriz_rotacion, (ancho, altura))
 
             # Si deseas guardar la imagen rotada en un archivo
-            cv2.imwrite(f"./NewDataSet3/{img[:6]}_{angulo_rotacion:.2f}.bmp", imagen_rotada)
-
+            cv2.imwrite(f"./NewDataSet3/{img[:3]+formatNum(num)}.bmp", imagen_rotada)
+            
+            
             angulo_rotacion +=19
 
             if angulo_rotacion >340 and angulo_rotacion< 342:
                 cont +=1 
                 print(f"Imagen {img} rotada completa. Total: {cont}")
-                
+df_Nph2.to_excel("./PH2_Extend.xlsx",index=False)
+
